4fe84eb9032308f72daa6eba5350f8e4284fe63a
abdalghani0 <117361839+abdalghani0@users.noreply.github.com>
2024-02-13 18:51:44 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import Script from "next/script";
-import Head from "next/head";
 
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -20,11 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
-      <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></Script>
-      <Script src="/oneSignal.js"></Script>
-    
-
+  
       <body className={space_Grotesk.className}>
         <ThemeProvider
           attribute="class"
--- public/oneSignal.js
+++
@@ -1,6 +0,0 @@
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-      OneSignalDeferred.push(function(OneSignal) {
-        OneSignal.init({
-          appId: "44c7436d-fc8f-42d6-a66b-d41c797f14ab",
-        });
-      });
