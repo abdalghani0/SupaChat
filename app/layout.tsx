7c5277fd53d4d71f361cb7abfc4f15e75dfe066c
abdalghani0 <117361839+abdalghani0@users.noreply.github.com>
2024-02-13 14:18:18 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <Head>
+
       <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></Script>
       <Script src="public/oneSignal"></Script>
-    </Head>
     
 
       <body className={space_Grotesk.className}>
