0496a249a418bca500f7e553e6d6e39147e8fbc9
abdalghani0 <117361839+abdalghani0@users.noreply.github.com>
2024-05-04 16:35:23 +0300

    changed chat about page

--- components/ChatAbout.tsx
+++ components/ChatAbout.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
+import WelcomeMessage from "./WelcomeMessage";
+import ChatInput from "./ChatInput";
+import { Transition } from "@headlessui/react";
+import { Input } from "./ui/input";
 
 function ChatAbout() {
-    return (
-        <div className="flex-1 flex items-center justify-center">
-            <div className="text-center space-y-5">
-                <h1 className="text-2xl md:text-3xl font-bold">Welcome to SupaChat</h1>
-                <p className="w-96">
-                    This is a chat application that is powered by supabase realtime db.
-                    Login to send messages.
-                </p>
-            </div>
-        </div>
-    );
+  const welcomeMessage = {};
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+  setTimeout(() => setShow1(true), 0);
+  setTimeout(() => setShow2(true), 1200);
+  setTimeout(() => setShow3(true), 2400);
+
+  return (
+    <>
+      <div className="flex-1 flex flex-col gap-7 p-5 h-full overflow-y-auto">
+        <div className="flex-1"></div>
+        <Transition
+          show={show1}
+          enter="transition-all duration-1000"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+        >
+          <WelcomeMessage message="Welcome to SupaChat!" />
+        </Transition>
+        <Transition
+          show={show2}
+          enter="transition-all duration-1000"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+        >
+          <WelcomeMessage message="This is a chat application that is powered by supabase realtime db" />
+        </Transition>
+        <Transition
+          show={show3}
+          enter="transition-all duration-1000"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+        >
+          <WelcomeMessage message=" Login to send messages" />
+        </Transition>
+      </div>
+      <div className="px-5 py-3">
+        <Input placeholder="send message" />
+      </div>
+    </>
+  );
 }
 
 export default ChatAbout;
---
+++ components/WelcomeMessage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+export default function WelcomeMessage({ message }: { message: string }) {
+  return (
+    <div
+      className={`flex gap-2 w-fit max-w-md bg-gradient-to-r from-pink-700 to-purple-700 rounded-md p-3 rounded-md transision-bg duration-500`}
+    >
+      <div className="shrink-0">
+        <Image
+          src="/chat.jpg"
+          alt="logo"
+          width={40}
+          height={40}
+          className="rounded-full ring-2"
+        />
+      </div>
+
+      <div className="flex-1 flex-col justify-end">
+        <h1>SupaChat</h1>
+        <p dir="auto" className={`text-gray-300 w-fit`}>
+          {message}
+        </p>
+      </div>
+    </div>
+  );
+}
